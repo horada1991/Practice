f9ee95f21ee1518d1b20ec1ed72ec5a68aa6e94f
Adam Horvath <horada1991@gmail.com>
2016-05-07 18:30:20 +0200

    How many numbers and right alaign added

--- Fibonacci_v2.py
+++ Fibonacci_v2.py
@@ -5,7 +5,7 @@ Fibo = [0, 1]
 num = int(input("How many Fibonacci number should I print out? "))
 for i in range(0, num):
     try:
-        print(str(i+1) + ".\t" + str(Fibo[i]).rjust(60))
+        print(str(i+1) + ".\t" + str(Fibo[i]).rjust(80))
     except IndexError:
         Fibo.append(Fibo[i-2]+Fibo[i-1])
-        print(str(i+1) + ".\t" + str(Fibo[i]).rjust(60))
+        print(str(i+1) + ".\t" + str(Fibo[i]).rjust(80))
