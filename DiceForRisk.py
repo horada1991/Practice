219a524751c0aca4ca38c4075570706004f1200f
Adam Horvath <horada1991@gmail.com>
2016-05-07 19:07:51 +0200

    Unit losts tracked

--- DiceForRisk.py
+++ DiceForRisk.py
@@ -12,6 +12,14 @@ dice_a.sort()
 dice_a.reverse()
 dice_d.sort()
 dice_d.reverse()
+print("Dice\n  Attacker:", "-".join(dice_a), "\n  Defender:", "-".join(dice_d), "\n")
 
+lost_d = 0
+lost_a = 0
+for i in range(0, len(dice_d)):
+    if int(dice_d[i]) >= int(dice_a[i]):
+        lost_a += 1
+    else:
+        lost_d +=1
 
-print("Dice\n  Attacker:", "-".join(dice_a), "\n  Defender:", "-".join(dice_d))
+print("Outcome:\n  Attacker: Lost", lost_a, "unit\n  Defender: Lost", lost_d, "unit\n")
