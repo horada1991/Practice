2d7379c68faecd129eee91015e772d5be192698b
Adam Horvath <horada1991@gmail.com>
2016-05-07 13:11:58 +0200

    For loop's range() changed

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,8 +2,9 @@
 
 Fibo = [0, 1]
 
-for i in range(1, 30):
-    Fibo.append(Fibo[i-1]+Fibo[i])
+for i in range(0, 28):
+    Fibo.append(Fibo[i]+Fibo[i+1])
 
 for i in range(0,30):
     print(str(i+1) + ". " + str(Fibo[i]))
+print(Fibo)
