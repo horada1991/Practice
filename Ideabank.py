628710735a1a6de22e9cddff961b80f658e92a97
Adam Horvath <horada1991@gmail.com>
2016-05-07 20:37:32 +0200

    --list argument added

--- Ideabank.py
+++ Ideabank.py
@@ -1,10 +1,23 @@
 
-idea = input("What is your new idea: ")
-with open("ideabank.md", "a+") as f:
-    f.write(idea + "\n")
-    print("\nYour ideabank:")
-    f.seek(0)
-    i = 1
-    for line in f:
-        print(str(i) + ". ", line, end = "")
-        i += 1
+import sys
+
+if sys.argv[1:]:
+    if sys.argv[1] == "--list":
+        with open("ideabank.md", "r") as f:
+            print("Your ideabank:")
+            i = 1
+            for line in f:
+                print(str(i) + ". ", line, end = "")
+                i += 1
+    else:
+        print("Unknown arguments!")
+else:
+    idea = input("What is your new idea: ")
+    with open("ideabank.md", "a+") as f:
+        f.write(idea + "\n")
+        print("\nYour ideabank:")
+        f.seek(0)
+        i = 1
+        for line in f:
+            print(str(i) + ". ", line, end = "")
+            i += 1
