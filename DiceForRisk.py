4b60c8e9f6024408f379747acd94257f8bd1a5ee
Adam Horvath <horada1991@gmail.com>
2016-05-07 19:29:29 +0200

    Changed for loops for random list gen.

--- DiceForRisk.py
+++ DiceForRisk.py
@@ -11,16 +11,14 @@ while 1:
         if int(units_d) > 0 and int(units_d) < 3:
             break
 
-dice_a = []
-dice_d = []
+dice = []
+for i in range(0,5):
+    dice.append(str(random.randrange(1,7)))
 
-for i in range(0,3):
-    dice_a.append(str(random.randrange(1,7)))
-for i in range(0,2):
-    dice_d.append(str(random.randrange(1,7)))
-
+dice_a = dice[:3]
 dice_a.sort()
 dice_a.reverse()
+dice_d = dice[3:]
 dice_d.sort()
 dice_d.reverse()
 print("Dice\n  Attacker:", "-".join(dice_a), "\n  Defender:", "-".join(dice_d), "\n")
@@ -32,5 +30,4 @@ for i in range(0, len(dice_d)):
         lost_a += 1
     else:
         lost_d += 1
-
 print("Outcome:\n  Attacker: Lost", lost_a, "unit\n  Defender: Lost", lost_d, "unit\n")
